8816976fdedf1c7c817f88f96fcf39edac659c9d
FrancoAguirre644 <francoaguirre644@gmail.com>
2022-01-28 01:21:59 -0300

    Añado template + Login Page.

--- assets/app.js
+++ assets/app.js
@@ -6,11 +6,48 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
-import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
+import './styles/app.css';
+
+import './css/style.css';
+
+import './vendors/mdi/css/materialdesignicons.min.css';
+
+import './vendors/css/vendor.bundle.base.css';
+
+import $ from 'jquery';
+
+$(document).on('mouseenter mouseleave', '.sidebar .nav-item', function(ev) {
+    var body = $('body');
+    var sidebarIconOnly = body.hasClass("sidebar-icon-only");
+    var sidebarFixed = body.hasClass("sidebar-fixed");
+    if (!('ontouchstart' in document.documentElement)) {
+      if (sidebarIconOnly) {
+        if (sidebarFixed) {
+          if (ev.type === 'mouseenter') {
+            body.removeClass('sidebar-icon-only');
+          }
+        } else {
+          var $menuItem = $(this);
+          if (ev.type === 'mouseenter') {
+            $menuItem.addClass('hover-open')
+          } else {
+            $menuItem.removeClass('hover-open')
+          }
+        }
+      }
+    }
+  });
+  $('.aside-toggler').click(function(){
+    $('.chat-list-wrapper').toggleClass('slide')
+  });
+
+/*
+import '../scss/sb-admin-2.scss';
+
+import '../vendor/datatables/dataTables.bootstrap4.min.css'
+// import 'https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css'
+import '../vendor/fontawesome-free/css/all.min.css'
 
 // start the Stimulus application
 import './bootstrap';
-
-bsCustomFileInput.init();
+*/
