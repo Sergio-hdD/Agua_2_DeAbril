536401860d8ea199b16161de6e04911c5a1dff16
FrancoAguirre644 <francoaguirre644@gmail.com>
2022-01-28 21:15:55 -0300

    Dashboard/Home agregado.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,11 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+/**
+  * @Route("/")
+  * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
+*/
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @IsGranted("ROLE_USER")
      */
     public function index(): Response
     {
